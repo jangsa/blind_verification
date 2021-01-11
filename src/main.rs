ab2c734549205fe0c4b78f9792518b1b55eb9ff1
jangsa <gute.ist.wieder.tot@gmail.com>
2021-01-11 16:37:31 +0900

    Json IO handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
+use std::collections::HashMap;
 use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
+// Verified Credential VC01's Format
 #[derive(Deserialize)]
-struct Info {
-    sub: String,
+struct Vc01In04 {
     name: String,
     age: i32,
+    job: String,
+}
+
+#[derive(Deserialize)]
+struct Vc01In03 {
+    profile: Vc01In04,
+}
+
+#[derive(Deserialize)]
+struct Vc01In02 {
+    credentialSubject: Vc01In03,
+
+    #[serde(flatten)]
+    extra: HashMap<String, Value>,
+}
+
+#[derive(Deserialize)]
+struct Vc01In01 {
+    sub: String,
+    jti: String,
+    iss: String,
+    nbf: u32,
+    iat: u32,
+    exp: u32,
+    nonce: String,
+    vc: Vc01In02,
+}
+
+// Output Json Format of "json" path
+#[derive(Serialize)]
+struct OutputJson {
+    sub: String,
+    age: i32,
+    adid: String,
 }
 
 #[get("/")]
@@ -19,8 +55,8 @@ async fn echo(req_body: String) -> impl Responder {
 }
 
 #[post("/json")]
-async fn json(info: web::Json<Info>) -> impl Responder {
-    format!("Welcome, {}! Your subject id is {}, and age is {}", info.name, info.sub, info.age)
+async fn json(vc01_in01: web::Json<Vc01In01>) -> impl Responder {
+    web::Json(OutputJson { adid: "testid".to_string(), sub: vc01_in01.sub.clone(), age: vc01_in01.vc.credentialSubject.profile.age})
 }
 
 #[get("/{id}/{name}")]
